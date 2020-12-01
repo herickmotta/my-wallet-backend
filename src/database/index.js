13940246d0f0d8fc03db9718b6f377ce36efac80
herick <herickrossato@gmail.com>
2020-12-01 09:59:10 -0300

    initial structure

---
+++ src/database/index.js
@@ -0,0 +1,13 @@
+const { Pool } = require('pg');
+require('dotenv').config();
+
+const connection = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD
+  });
+
+
+module.exports = connection;
