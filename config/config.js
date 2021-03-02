1ee0bca90ffc7ae6e5f2865f5ff8f5ec97284ebe
herickmotta <herickrossato@gmail.com>
2021-03-01 21:01:42 -0300

    fix deploy

--- config/config.js
+++ config/config.js
@@ -1,13 +1,27 @@
-require("dotenv").config();
+dotenv.config();
 
 module.exports = {
   development: {
     url: process.env.DATABASE_URL,
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: false,
+        rejectUnauthorized: false,
+      },
+    },
   },
   test: {
     url: process.env.DATABASE_URL,
   },
   production: {
     url: process.env.DATABASE_URL,
-  }
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: false,
+        rejectUnauthorized: false,
+      },
+    },
+  },
 };
